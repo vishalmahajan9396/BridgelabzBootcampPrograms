a1b4e8a5692fb1beecc06665e8258276f43f7595
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-10 19:15:29 +0530

    Employee is Present or not is checked by using RANDOM in EmployeeWageComputation.sh

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,3 +1,12 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program";
+
+randomCheck=$(( RANDOM%2 ));
+isPresent=1;
+if [ $isPresent -eq $randomCheck ];
+then
+        echo "Employee is Present";
+else
+        echo "Employee is not Present";
+fi
