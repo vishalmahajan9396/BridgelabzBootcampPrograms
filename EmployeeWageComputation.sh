fd699928d67513f9f7a06d16b7f9274d57da4bfd
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-10 19:53:39 +0530

    Daily wage and total wage are store in array in EmployeeWageComputation.sh

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -15,10 +15,10 @@ function getWorkHrs() {
    local $empCheck=$1;
    case $empCheck in
       $IS__FULL_TIME)
-         empHrs=8
+         empHrs=12
          ;;
       $IS_PART_TIME)
-         empHrs=4
+         empHrs=8
          ;;
       *)
          empHrs=0
@@ -27,14 +27,21 @@ function getWorkHrs() {
    echo $empHrs;
 }
 
+function getEmpWage() {
+   local empHr=$1;
+   echo $(($empHr*$EMP_RATE_PER_HR));
+}
+
 while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
 	((totalWorkingDays++));
 	empCheck=$((RANDOM%3));
 	empHrs="$(getWorkHrs $empCheck)";
 	totalEmpHrs=$(($totalEmpHrs+$empHrs));
+	dailyWage[$totalWorkingDays]="$( getEmpWage $empHrs )";
 done
 
 totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR ));
 echo "Total Employee Hours: "$totalEmpHrs;
 echo "Total Employee Salary: "$totalSalary;
+echo ${dailyWage[@]};
