6dc8fcbfecb8c7e8a3b86596f113a6d4fcd572e2
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-10 19:41:55 +0530

    Wages are calculated till working hours or days is reached for month in EmployeeWageComputation.sh

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -2,28 +2,33 @@
 
 echo "Welcome to Employee Wage Computation Program";
 
-isPartTime=1;
-isFullTime=2;
-wagePerHour=20;
-workingDayPerMonth=20;
-totalSalary=0;
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-for (( day=1; day<=$workingDayPerMonth; day++ ))
+totalEmpHrs=0;
+totalWorkingDays=0;
+
+while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
 do
-	randomCheck=$(( RANDOM%3 ));
-	case $randomCheck in
-		$isFullTime)
-			fullDayHour=12;
-			;;
-		$isPartTime)
-			fullDayHour=8;
-			;;
-		*)
-			fullDayHour=0;
-			;;
-	esac
-
-	salary=$(( $wagePerHour * $fullDayHour ));
-	totalSalary=$(( $totalSalary + $salary ));
+	((totalWorkingDays++));
+	empCheck=$((RANDOM%3));
+		case $empCheck in
+			$IS_FULL_TIME)
+				empHrs=12
+				;;
+			$IS_PART_TIME)
+				empHrs=8
+				;;
+			*)
+				empHrs=0
+				;;
+		esac
+	totalEmpHrs=$(($totalEmpHrs+$empHrs));
 done
-echo "Total Salary: "$totalSalary;
+
+totalSalary=$(( $totalEmpHrs * $EMP_RATE_PER_HR ));
+echo "Total Employee Hours: "$totalEmpHrs;
+echo "Total Employee Salary: "$totalSalary;
