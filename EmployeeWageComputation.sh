263144d62f5e2dc229c50a39ed24c1e7c86580f7
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-10 19:36:42 +0530

    Wages for month are calculated in EmployeeWageComputation.sh

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -5,19 +5,25 @@ echo "Welcome to Employee Wage Computation Program";
 isPartTime=1;
 isFullTime=2;
 wagePerHour=20;
-randomCheck=$(( RANDOM%3 ));
+workingDayPerMonth=20;
+totalSalary=0;
 
-case $randomCheck in
-	$isFullTime)
-		fullDayHour=12;
-		;;
-	$isPartTime)
-		fullDayHour=8;
-		;;
-	*)
-		fullDayHour=0;
-		;;
-esac
+for (( day=1; day<=$workingDayPerMonth; day++ ))
+do
+	randomCheck=$(( RANDOM%3 ));
+	case $randomCheck in
+		$isFullTime)
+			fullDayHour=12;
+			;;
+		$isPartTime)
+			fullDayHour=8;
+			;;
+		*)
+			fullDayHour=0;
+			;;
+	esac
 
-salary=$(( $wagePerHour * $fullDayHour ));
-echo "Salary: "$salary;
+	salary=$(( $wagePerHour * $fullDayHour ));
+	totalSalary=$(( $totalSalary + $salary ));
+done
+echo "Total Salary: "$totalSalary;
