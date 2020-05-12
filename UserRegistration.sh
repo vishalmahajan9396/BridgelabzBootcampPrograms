0bdd5303b80645c85cf65f258aba922da2b6ccd7
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-12 14:43:15 +0530

    First name of user added in UserRegistration.sh

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration";
+read -p "Enter First Name:" firstName;
+firstNamePattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
+if [[ $firstName =~ $firstNamePattern ]];
+then
+	echo "First name is:" $firstName;
+else
+	echo "Enter Valid name";
+fi
