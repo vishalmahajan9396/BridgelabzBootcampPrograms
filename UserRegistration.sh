aacbd90fbdd70182e832893c05a14e4aefa86a4f
vishalmahajan9396 <vshalmahajan9396@gmail.com>
2020-05-12 14:49:47 +0530

    Last name added in UserRegistration.sh

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
 echo "Welcome to User Registration";
+
 read -p "Enter First Name:" firstName;
 firstNamePattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
 if [[ $firstName =~ $firstNamePattern ]];
@@ -8,4 +9,13 @@ then
 	echo "First name is:" $firstName;
 else
 	echo "Enter Valid name";
+fi
+
+read -p "Enter Last Name:" lastName;
+lastNamePattern="^[[:upper:]]{1}[[:lower:]]{2,}$";
+if [[ $lastName =~ $lastNamePattern ]];
+then
+   echo "Last name is:" $lastName;
+else
+   echo "Enter Valid name";
 fi
